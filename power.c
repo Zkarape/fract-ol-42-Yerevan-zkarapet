8454b0e5ca8aa7e0107706c196e9a86023e9287b
zhenkar2003 <zhenya.k.y@tumo.org>
2022-07-26 18:51:17 +0400

    zheno

--- power.c
+++ power.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-double	power(double x, double n)
+double	power(double x, int n)
 {
 	double	res;
 
 	res = 1;
-	while (n >= 0)
-	{
+	while (n--)
 		res = res * x;
-		n -= 1;
-	}
 	return (res);
 }
 
@@ -27,20 +24,42 @@ double	sqroot(double x)
 	return (sqrt);
 }
 
-double	v_formula(mlx strct)
+double	find_mod(double a, double b)
+{
+	double	mod;
+
+	mod = a;
+	if (b < 0)
+		b = -b;
+	while (mod > b)
+		mod = mod - b;
+	return (mod);
+}
+
+double	v_formula(double iter_count, double maxIter, double N)//N is the count of palette items
 {
 	double	v;
+	double	d;
+	double res;
 
+	d = power((iter_count / maxIter), 3);
+	printf("d == %f\n", d);
+	res = power(d * N, 2) / sqroot(d * N);
+	printf("res == %f %f %f\n", d * N, power(d * N, 2), sqroot(d * N));
+	v = find_mod(res, N);
+	printf("v == %f\n", v);
+	return (v);
 }
 
-double	color_magic()
-{
-
-}
+//double	color_magic()
+//{
+//
+//}
 
 int main()
 {
-	sqroot(9);
-//	printf("%f\n", power(4, 1.5));
+//	sqroot(9);
+	printf("%f\n", v_formula(20, 50, 4));
+//	printf("%f\n", power(4, 2));
 //	printf("%f\n", sqroot(9));
 }
